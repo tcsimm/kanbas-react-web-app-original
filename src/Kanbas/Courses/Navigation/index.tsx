79e1c868efc9209f208a80e9e4d26e6d4a4b79a6
simmonsthomas <simmons.th@northeastern.edu>
2024-07-01 14:03:36 -0400

    Add Kanbas components and update routing

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,16 +1,15 @@
-// src/Kanbas/Courses/Navigation/index.tsx
 import React from 'react';
 
-export default function CoursesNavigation() {
+export default function KanbasNavigation() {
   return (
-    <ul id="wd-courses-navigation">
-      <li><a id="wd-course-home-link" href="#/Kanbas/Courses/1234/Home">Home</a></li>
-      <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/1234/Modules">Modules</a></li>
-      <li><a id="wd-course-piazza-link" href="#/Kanbas/Courses/1234/Piazza">Piazza</a></li>
-      <li><a id="wd-course-zoom-link" href="#/Kanbas/Courses/1234/Zoom">Zoom</a></li>
-      <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/1234/Assignments">Assignments</a></li>
-      <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/1234/Quizzes">Quizzes</a></li>
-      <li><a id="wd-course-grades-link" href="#/Kanbas/Courses/1234/Grades">Grades</a></li>
+    <ul id="wd-kanbas-navigation">
+      <li><a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">Northeastern</a></li>
+      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
+      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
+      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
+      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
+      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
+      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
     </ul>
   );
 }
