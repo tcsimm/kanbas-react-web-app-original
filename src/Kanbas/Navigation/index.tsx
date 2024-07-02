615d7c81207b6f1a73a29571d59a6bfe22942fbb
simmonsthomas <simmons.th@northeastern.edu>
2024-07-02 07:43:44 -0400

    e

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,13 +1,19 @@
+// src/Kanbas/Navigation/index.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function KanbasNavigation() {
   return (
-    <ul id="wd-kanbas-navigation">
-      <li><a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/">Northeastern</a></li>
-      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
-      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
-      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
-      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
-      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
-      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
-    </ul>
-);}
-
+    <nav>
+      <ul>
+        <li><Link to="/">Northeastern</Link></li>
+        <li><Link to="/Account">Account</Link></li>
+        <li><Link to="/Dashboard">Dashboard</Link></li>
+        <li><Link to="/Courses">Courses</Link></li>
+        <li><Link to="/Calendar">Calendar</Link></li>
+        <li><Link to="/Inbox">Inbox</Link></li>
+        <li><Link to="/Labs">Labs</Link></li>
+      </ul>
+    </nav>
+  );
+}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,3 +1,4 @@
+// src/Kanbas/index.tsx
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './Dashboard';
@@ -8,20 +9,21 @@ export default function Kanbas() {
   return (
     <div id="wd-kanbas">
       <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <KanbasNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Dashboard" />} />
-                <Route path="Dashboard" element={<Dashboard />} />
-                <Route path="Courses/*" element={<Courses />} />
-              </Routes>
-            </td>
-          </tr>
-        </tbody>
+        <tr>
+          <td valign="top">
+            <KanbasNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Dashboard" element={<Dashboard />} />
+              <Route path="Courses/*" element={<Courses />} />
+              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="Calendar" element={<h1>Calendar</h1>} />
+              <Route path="Inbox" element={<h1>Inbox</h1>} />
+            </Routes>
+          </td>
+        </tr>
       </table>
     </div>
   );
