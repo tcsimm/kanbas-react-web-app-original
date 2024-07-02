f0a73856250c55f3930a40869c39d71fded6865e
simmonsthomas <simmons.th@northeastern.edu>
2024-07-01 21:52:19 -0400

    p

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Kanbas from './Kanbas';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<Kanbas />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Kanbas />
+    </div>
   );
 }
 
