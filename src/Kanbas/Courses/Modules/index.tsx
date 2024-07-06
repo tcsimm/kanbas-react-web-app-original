bd6d1a25c8f9572bcfd8df2cbb7e6cae8d0b1fbd
tcsimm <widet77@icloud.com>
2024-07-06 08:32:30 -0400

    modules

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-
+// src/Kanbas/Courses/Modules/index.tsx
 export default function Modules() {
   return (
     <div>
-      <h2>Modules</h2>
-      <ul>
-        <li>Module 1: Introduction</li>
-        <li>Module 2: Advanced Topics</li>
-        <li>Module 3: Final Project</li>
+      {/* Buttons for module actions */}
+      <div className="wd-modules-actions">
+        <button>Collapse All</button>
+        <button>View Progress</button>
+        <select>
+          <option>Publish All</option>
+          <option>Unpublish All</option>
+        </select>
+        <button>+ Module</button>
+      </div>
+      <ul id="wd-modules">
+        <li className="wd-module">
+          <div className="wd-title">Week 1</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to the course</li>
+                <li className="wd-content-item">Learn what is Web Development</li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
+                <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating User Interfaces</li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to Web Development</li>
+                <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                <li className="wd-content-item">Creating a React Application</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className="wd-module">
+          <div className="wd-title">Week 2</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
